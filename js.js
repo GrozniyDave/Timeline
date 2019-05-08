24e3a8094015787496233d82c3dde1e81e38fe03
GrozniyDave <38036338+GrozniyDave@users.noreply.github.com>
2019-05-08 20:23:11 +0200

    images?

--- js.js
+++ js.js
@@ -5,9 +5,17 @@ function showAlbum() {
   let a = document.querySelector(".trigger");
   if (x.style.display === "none") {
     x.style.display = "block";
-    a.innerHTML = "Hide Albums";
+    a.innerHTML = "Hide Albums  <i class='fa fa-close'></i> ";
   } else {
     x.style.display = "none";
-    a.innerHTML = "Show Albums";
+    a.innerHTML = "Show Albums  <i class='fa fa-expand'></i>";
   }
 }
+
+let facts = [
+  'Album "One-X" was released on June 13, 2006',
+  'Release date of "Transit of Venus" is October 2, 2012',
+  '"Outsider" was released on March 9, 2018',
+  'The first album with Matt as a lead singer was "Human" and it was released on March 31, 2015',
+  'The album "Life Starts Now" was released on September 22, 2009'
+];
