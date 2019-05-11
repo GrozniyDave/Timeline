d6cb84b10e2dddefff2968fe10d038e1e8e8d095
GrozniyDave <38036338+GrozniyDave@users.noreply.github.com>
2019-05-11 16:23:12 +0200

    animations,readMore buttons added

--- js.js
+++ js.js
@@ -1,5 +1,56 @@
 "use strict";
+// let card = document.getElementsByClassName("timeline-item__content-wrapper");
 
+//when hovering headline div function
+// function whenHover() {
+//   for (let i = 0; i < card.length; i++) {
+//     card[i].addEventListener("onclick", showContent());
+//   }
+// }
+
+//showing image and descrption function
+// function showContent() {
+//   let image = document.getElementsByClassName("timeline-item__image");
+//   let desc = document.getElementsByClassName("timeline-item__body");
+
+//   for (var i = 0; i < image.length; i += 1) {
+//     image[i].style.display = "block";
+//     image[i].style.opacity = "1";
+//   }
+//   for (var i = 0; i < desc.length; i += 1) {
+//     desc[i].style.display = "block";
+//     desc[i].style.opacity = "1";
+//   }
+// }
+
+// function hideContent() {
+//   let image = document.getElementsByClassName("timeline-item__image");
+//   let desc = document.getElementsByClassName("timeline-item__body");
+
+//   for (var i = 0; i < image.length; i += 1) {
+//     image[i].style.display = "none";
+//     image[i].style.opacity = "0";
+//   }
+//   for (var i = 0; i < desc.length; i += 1) {
+//     desc[i].style.display = "none";
+//     desc[i].style.opacity = "0";
+//   }
+// }
+//animateCSS handling function
+function animateCSS(element, animationName, callback) {
+  const node = document.querySelector(element);
+  node.classList.add("animated", animationName);
+
+  function handleAnimationEnd() {
+    node.classList.remove("animated", animationName);
+    node.removeEventListener("animationend", handleAnimationEnd);
+
+    if (typeof callback === "function") callback();
+  }
+
+  node.addEventListener("animationend", handleAnimationEnd);
+}
+//trigering displaying/hiding albums function
 function showAlbum() {
   let nav = document.querySelector(".absolute-nav");
   let button1 = document.querySelector(".trigger");
@@ -7,10 +58,12 @@ function showAlbum() {
     nav.style.display = "block";
     button1.innerHTML = "Hide Albums  <i class='fa fa-close'></i> ";
   } else {
+    animateCSS(".absolute-nav", "zoomInDown");
     nav.style.display = "none";
     button1.innerHTML = "Show Albums  <i class='fa fa-expand'></i>";
   }
 }
+//trigering displaying/hiding facts function
 
 function showQuotes() {
   let right = document.querySelector(".rightbar");
@@ -23,6 +76,7 @@ function showQuotes() {
     button2.innerHTML = "Show Facts  <i class='fa fa-expand'></i> ";
     console.log("You've just hidden the Facts!!");
   } else {
+    animateCSS(".rightbar", "zoomInDown");
     console.log("Facts are displayed!)");
     right.style.display = "block";
     right.classList.add("rightbar");
@@ -30,10 +84,13 @@ function showQuotes() {
     left.classList.add("lefttbar");
     right.style.display = "";
     left.style.display = "";
+    animateCSS(".leftbar", "zoomInDown");
+
     button2.innerHTML = "Hide Facts  <i class='fa fa-close'></i> ";
   }
 }
 
+//array of facts
 let facts = [
   "Three Days Grace are Neil Sanderson (drums, piano, backing vocals), Brad Walst (bass guitar), and Barry Stock (lead guitar). Matt Walst (lead vocals)",
   "The group has sold more than 6 million records in the United States",
@@ -56,15 +113,14 @@ let facts = [
   "“I Hate Everything About You” peaked at #2 on Modern and Active chart for 47 weeks (2003)"
 ];
 
+//ScrollReveal animation appears on page load
 ScrollReveal().reveal(".absolute-nav", { delay: 400 });
 ScrollReveal().reveal(".trigger", { delay: 600 });
 ScrollReveal().reveal(".trigger1", { delay: 600 });
-
 ScrollReveal().reveal(".dot__graphic", { delay: 1000 });
 ScrollReveal().reveal(".timeline-item__line", { delay: 1000 });
-
 ScrollReveal().reveal(".timeline-item__header", { delay: 1000 });
-ScrollReveal().reveal(".timeline-item__image", { delay: 1100 });
+ScrollReveal().reveal(".timeline-item__image", { delay: 1000 });
 ScrollReveal().reveal(".timeline-item__body", { delay: 1200 });
 ScrollReveal().reveal(".quote", { delay: 1300 });
 ScrollReveal().reveal(".icon-scroll", { delay: 3000 });
